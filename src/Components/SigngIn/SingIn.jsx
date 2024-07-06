57529760d0d71dae677dd5a399d5d9c0d0676feb
jose22gonzalez <urenajosealbeto@gmail.com>
2024-07-06 14:49:00 -0400

    CS - 55 fixed import

--- src/Components/SigngIn/SingIn.jsx
+++ src/Components/SigngIn/SingIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useContextCar } from "../Context/Context";
+import { useContextCar } from "../../Context/Context";
 import { useNavigate, Link } from "react-router-dom"
 const SignIn = () => {
 
