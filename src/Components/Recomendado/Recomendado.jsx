8e79d1e55687ca18e9fb3f6e35a9a3534920b776
Julio Cesar <jimeno.juliocesar@gmail.com>
2024-07-19 23:05:41 -0400

    CS-59-Enhanced-Form-Functionality-Automatic-Clearing-Status-Notifications-and-User-Friendly-Modals

--- src/Components/Recomendado/Recomendado.jsx
+++ src/Components/Recomendado/Recomendado.jsx
@@ -1,9 +1,11 @@
+
 import React, { useState, useEffect } from 'react';
 import Modal from './modal';
 import { useContextCar } from '../../Context/Context';
 import { FaEdit } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { FaPlus } from "react-icons/fa6";
+
 import { GoChevronRight } from "react-icons/go";
 import { MdDelete } from 'react-icons/md';
 
@@ -11,20 +13,31 @@ const Recomendado = () => {
     const { user, WhichRole, ListCar, setListCar, SerchingCar,
         setAvailable, isFiltro, setCarEdit, Formatnumber, DeleteCarSale,
         isOpenCardDetails, setisOpenCardDetails, ListCarSale, setLisCarNew, setLisCarUsed, EditCarSale
-    } = useContextCar();
-
-
+    } = useContextCar()
     const [showModal, setShowModal] = useState(false);
-
-
-    const [SeeCar, setSeeCar] = useState([]);
     const [showAll, setShowAll] = useState(false);
-    
+
+    const [SeeCar, setSeeCar] = useState([])
     const navigate = useNavigate();
+
+    const handleOpenModal = (car) => {
+        setAvailable(car)
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
     const handleAgregarAuto = () => {
         setisOpenCardDetails(!isOpenCardDetails);
     }
+
+    const handleEditAuto = (car) => {
+        setCarEdit(car);
+        setisOpenCardDetails(!isOpenCardDetails);
+    }
+
 
     useEffect(() => {
         if (isFiltro) {
@@ -33,18 +46,6 @@ const Recomendado = () => {
             setSeeCar([...ListCar]);
         }
     }, [isFiltro, ListCar, SerchingCar]);
-
-
-
-    const handleEditAuto = (car) => {
-        setCarEdit(car);
-        setisOpenCardDetails(!isOpenCardDetails);
-    }
-
-    // const handleDelete = async (carSaleId) => {
-    //     await DeleteCarSale(carSaleId);
-    //     setListCar(ListCar);
-    // }
 
     const handleDelete = async (carSaleId) => {
         try {
@@ -72,22 +73,21 @@ const Recomendado = () => {
 
         }
     }
-    const handleOpenModal = (car) => {
-        setAvailable(car)
-        setShowModal(true);
-    };
-
-
 
     const displayedCars = showAll ? SeeCar : SeeCar.slice(0, 9);
 
     return (
-        <div className="bg-transparent flex justify-center md:m-10 items-center xl:mt-36 max-md:px-5">
-            <div className="flex flex-col mt-96 justify-between w-full max-w-[992px] max-md:mt-10 max-md:max-w-full">
+        <div className="bg-transparent flex justify-center md:m-10 items-center xl:mt-36 max-md:px-5 bg-[#0B0C10]" >
+
+            <div className="flex flex-col z-10 mt-96  justify-between w-full max-w-[992px] max-md:mt-10 max-md:max-w-full">
+
+
                 <div className="flex">
-                    <div className="text-2xl mt-20 font-bold text-white">
+                    <div className="text-2xl mt-20 font-bold text-white ">
                         Autos disponibles
+
                     </div>
+
                 </div>
                 {SeeCar.length > 9 && (
                     <div className="flex w-full mt-3 px-6 py-3 justify-end font-bold lg:text-2xl">
@@ -101,23 +101,29 @@ const Recomendado = () => {
                 )}
                 <div className="mt-6 max-md:max-w-full">
                     <div className="">
-                        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+
+                        <div className="grid grid-cols-1 gap-4 md:grid-cols-3    ">
                             {user && (WhichRole === 'admin' || WhichRole === 'Owner') && (
                                 <div className="flex items-center justify-center text-2xl border text-white rounded transition duration-300 hover:bg-blue-500 hover:text-white focus:outline-none">
-                                    <div>
-                                        <button onClick={handleAgregarAuto}>
+
+                                    <div className="">
+                                        <button onClick={() => handleAgregarAuto()}
+                                        >
                                             <FaPlus className='mx-40 mt-4 text-4xl' />
-                                            <div className='text-4xl m-5'>Agregar auto nuevo</div>
+                                            <div className='text-4xl m-5 '>Agregar auto nuevo </div>
+
                                         </button>
                                     </div>
                                 </div>
                             )}
+
                             {displayedCars.map((car, index) => (
                                 <div key={index} className="flex flex-col max-md:ml-0 max-md:w-full">
                                     {user && (WhichRole === 'admin' || WhichRole === 'Owner') && (
                                         <div className='flex flex-row'>
                                             <div className="px-3 py-2 text-xs leading-4">
-                                                <button onClick={() => handleEditAuto(car)} className="px-3 py-1 border border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-yellow-400 hover:text-white focus:outline-none">
+                                                <button onClick={() => handleEditAuto(car)}
+                                                    className="px-3 py-1 border border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-yellow-400 hover:text-white focus:outline-none">
                                                     <FaEdit size={14} className="text-yellow-500" />
                                                 </button>
                                             </div>
@@ -144,11 +150,20 @@ const Recomendado = () => {
                                     </div>
                                 </div>
                             ))}
-                            
+
+
+
                         </div>
+
+
+
                     </div>
+
+
                 </div>
             </div>
+
+            <Modal showModal={showModal} handleClose={handleCloseModal} />
         </div>
     );
 };
--- src/Components/Recomendado/modal.jsx
+++ src/Components/Recomendado/modal.jsx
@@ -5,15 +5,14 @@ import { FaEdit } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useContextCar } from '../../Context/Context';
 
-const Modal = ({ showModal, handleClose, cars }) => {
+const Modal = ({  showModal, handleClose}) => {
 
-    const { user, WhichRole, CarAvailable} = useContextCar()
+    const { user, WhichRole,CarAvailable,Formatnumber } = useContextCar()
     const navigate = useNavigate();
 
-    
     const handlFormulario = () => {
         window.scrollTo(0,0);
-        navigate('/admin/DetailsAutos')
+        // navigate('/admin/DetailsAutos')
     }
 
     useEffect(() => {
@@ -28,61 +27,49 @@ const Modal = ({ showModal, handleClose, cars }) => {
     }, [showModal])
 
 
+
     return (
         <>
-            {showModal && 
-                <div className='fixed  inset-0 flex items-center justify-center z-50  sm:mx-0 min-h-screen w-full text-white backdrop-blur-sm ' >
+            {showModal &&
+                <div className='fixed inset-0 flex items-center justify-center z-50  sm:mx-0 min-h-screen w-full text-white backdrop-blur-sm ' >
                     <div className="relative bg-gray-900  rounded-lg overflow-hidden shadow-xl max-w-screen-md w-full m-4" >
                         <div className="flex flex-col px-7 w-full max-md:px-5 max-md:max-w-full m-4">
 
                             <div className="flex text-3xl mt-5 max-md:text-2xl font-extrabold text-white max-md:flex-wrap max-md:max-w-full">
-                                <div className="flex-auto">
-                                    {cars?.Sale?.DetalleCoche?.Titulo}
-                                
-                                </div>
+                                <div className="flex-auto">{CarAvailable?.Sale?.DetalleCoche?.Titulo}</div>
 
-                                <IoMdClose className='cursor-pointer mx-10 hover:bg-red-600 hover:rounded-full' onClick={() => showModal(false)}  />
+                                <IoMdClose className='cursor-pointer mx-10 hover:bg-red-600 hover:rounded-full' onClick={handleClose} />
                             </div>
                             <div className="flex gap-1.5 self-start mt-6 text-xl font-bold ">
-                                <div className="text-white">US$:  </div>
+                                <div className="text-white">US$: </div>
                                 <div className="text-gray-500 flex flex-row ">
-                                {/* {CarAvailable?.Sale?.Precio?.Precio} */}
-
-                                    {/* {user && (WhichRole === 'admin' || WhichRole === 'Owner') && (
-                                        <div className="px-3 py-2   text-xs leading-4">
-                                            <button className="px-3 py-1 border border-blue-500 text-blue-500 rounded transition duration-300 hover:bg-yellow-400 hover:text-white focus:outline-none">
-                                                <FaEdit size={14} className="text-yellow-400" />
-                                            </button>
-                                        </div>
-                                    )} */}
-
+                                   {/* {CarAvailable?.Sale?.Precio?.Precio} */}
+                                   <div className="">${Formatnumber(CarAvailable?.Sale?.Precio?.Precio)}</div>
+                                    
                                 </div>
                             </div>
                         </div>
 
-                                    
-
                         <div className="prose max-w-screen-md p-6 overflow-y-auto" style={{ maxHeight: '70vh', borderRadius: '0.375rem', boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.1)' }}>
 
-                            <div className=" xl:max-w-[50rem]  m-1">
+                            <div className=" xl:max-w-[50rem]  m-5">
 
                                 <div className=" w-full bg-white border-white border-solid border-[3px] min-h-[4px] max-md:max-w-full" />
-                                
                                 <div className="self-center mt-8 w-full max-w-[502px] max-md:mt-10 max-md:max-w-full">
                                     <div className="flex gap-5 max-md:flex-col max-md:gap-0">
                                         <div className="flex flex-col w-[56%] max-md:ml-0 max-md:w-full">
                                             <div className="flex flex-col grow whitespace-nowrap ">
                                                 <div className="flex gap-3.5">
                                                     <div className="text-2xl font-bold text-white max-md:text-xl">Color:</div>
-                                                    {/* <div className="text-2xl text-white text-opacity-50 max-md:text-xl">{CarAvailable?.Sale.Detalle}</div> */}
+                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">{CarAvailable?.Sale?.DetalleCoche?.Color}</div>
                                                 </div>
                                                 <div className="flex gap-3 mt-6">
                                                     <div className="text-2xl font-bold text-white max-md:text-xl">Año:</div>
-                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl"></div>
+                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">{CarAvailable?.Sale?.DetalleCoche?.Year}</div>
                                                 </div>
                                                 <div className="flex gap-3.5 mt-7">
                                                     <div className="text-2xl font-bold text-white max-md:text-xl">Combustible:</div>
-                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">Electrico</div>
+                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">{CarAvailable?.Sale?.DetalleMotor?.TipoCombustimble}</div>
                                                 </div>
                                             </div>
                                         </div>
@@ -90,11 +77,11 @@ const Modal = ({ showModal, handleClose, cars }) => {
                                             <div className="flex flex-col max-md:mt-10">
                                                 <div className="flex gap-3.5">
                                                     <div className="text-2xl font-bold text-white max-md:text-xl">Capacidad:</div>
-                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">5 / P</div>
+                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">{CarAvailable?.Sale?.DetalleCoche?.Capacidad} / P</div>
                                                 </div>
                                                 <div className="flex gap-3.5 mt-6 whitespace-nowrap ">
                                                     <div className="text-2xl font-bold text-white max-md:text-xl">Condición:</div>
-                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">Nuevo</div>
+                                                    <div className="text-2xl text-white text-opacity-50 max-md:text-xl">{CarAvailable?.Sale?.DetalleCoche?.Condicion}</div>
                                                 </div>
                                             </div>
                                         </div>
@@ -105,7 +92,7 @@ const Modal = ({ showModal, handleClose, cars }) => {
                                 <div className="flex flex-col mt-5 w-full text-white max-md:px-5 max-md:max-w-full">
                                     <div className="text-3xl max-md:max-w-full">Descripción</div>
                                     <div className="px-3 pt-4 pb-28 mt-4 text-xl rounded-lg bg-white bg-opacity-30 text-white text-opacity-90 max-md:pr-5 max-md:max-w-full ">
-                                        Disfruta de una cabina silenciosa gracias al vidrio acústico de 360 grados.
+                                    {CarAvailable?.Sale?.DetalleCoche?.Descripcion}
                                     </div>
                                     <div className="justify-center self-center px-7 py-4 mt-6 text-3xl  max-md:text-xl whitespace-nowrap rounded-lg bg-sky-600 hover:bg-sky-400 bg-opacity-60  max-md:px-5 cursor-pointer"
 
