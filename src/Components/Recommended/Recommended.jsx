5f35083975717101926f1c786b7968b332560907
StormySilverio05 <stormysilverionunez@gmail.com>
2024-08-30 20:40:27 -0700

    CS-73

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -100,16 +100,7 @@ const Recommended = ({refAutos}) => {
                 <div>
                     <FilterComponent />
                 </div>
-                {SeeCar.length > 9 && (
-                    <div className="flex w-full mt-3 px-6 py-3 justify-end font-bold lg:text-2xl">
-                        <div className="flex text-blue-500 items-center">
-                            <button onClick={() => setShowAll(!showAll)} className="text-[1rem]">
-                                {showAll ? 'Ver menos' : 'Ver más'}
-                            </button>
-                            <GoChevronRight className='w-[18px]' />
-                        </div>
-                    </div>
-                )}
+                
                 <div className="mt-6 max-md:max-w-full">
                     <div className="">
 
@@ -166,7 +157,56 @@ const Recommended = ({refAutos}) => {
 
 
                         </div>
+                        {SeeCar.length > 9 && (
+                        <div className="flex w-full mt-3 px-6 py-3 justify-center font-bold lg:text-2xl">
+                            <div class="flex items-center gap-2 ">
+                                <button
+                                    class="flex items-center gap-2  px-6 py-3 font-sans text-xs font-bold text-center md:text-2xl  text-blue-500 uppercase align-middle transition-all rounded-full select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className='md:h-8 md:w-8' fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                    aria-hidden="true" class="w-4 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+                                    </svg>
+                                    Anterior
+                                </button>
+                                <div class="flex items-center gap-1  ">
+                                    <button
+                                    class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button">
+                                    <span class="absolute md:text-3xl  text-blue-500 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                        1
+                                    </span>
+                                    </button>
+                                    <button
+                                    class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button">
+                                    <span class="absolute md:text-3xl  text-blue-500 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                        2
+                                    </span>
+                                    </button>
+                                    <button
+                                    class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full bg-gray-900 text-center align-middle font-sans text-xs font-medium uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button">
+                                    <span class="absolute md:text-3xl  text-blue-500 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                        3
+                                    </span>
+                                    </button>
+                                </div>
+                                <button
+                                    class="flex items-center md:text-2xl  gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-blue-500 uppercase align-middle transition-all rounded-full select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button">
+                                    Siguiente
+                                    <svg xmlns="http://www.w3.org/2000/svg" className='md:h-8 md:w-8' fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                    aria-hidden="true" class="w-4 h-4">
+                                    <path  stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+                                    </svg>
+                                </button>
+                            </div> 
 
+
+
+                        </div>
+                )}
 
 
                     </div>
